2cbda6775bc1a00dc3307a2309ef928c5fc60da9
Tom Narongsak <narongsakk59@gmail.com>
2024-11-17 03:36:39 +0700

    update history

--- src/pages/pvp/history.tsx
+++ src/pages/pvp/history.tsx
@@ -27,6 +27,7 @@ const HistoryPageStyle = styled.div`
 `;
 const HistoryPage = () => {
   const [history, setHistory] = useState<any[]>([]);
+  const [battleDetail, setBattleDetail] = useState<any>(null);
 
   useEffect(() => {
     getHistory();
@@ -39,30 +40,78 @@ const HistoryPage = () => {
       }
     });
   };
+
+  const getHistoryDetails = async (id: any) => {
+    await apiBattle.getBattleDetail(id).then((res: any) => {
+      if (res) {
+        setBattleDetail(res.data);
+      } else {
+        setBattleDetail(null);
+      }
+    });
+  };
+
   return (
-    <HistoryPageStyle>
-      <div className={'description'}>Battle history</div>
-      <HistoryListContainer>
-        <div className={'header'}>
-          <div className={'cell'}>No.</div>
-          <div className={'cell'}>Match ID</div>
-          <div className={'cell'}>Level</div>
-          <div className={'cell'}></div>
-        </div>
-        {history &&
-          history.map((res, index) => {
-            return (
-              <div key={index} className={'row'}>
-                <div className={'cell'}>1</div>
-                <div className={'cell'}>1</div>
-                <div className={'cell'}>1</div>
-                <div className={'cell'}>1</div>
-                <div className={'cell'}>1</div>
-              </div>
-            );
-          })}
-      </HistoryListContainer>
-    </HistoryPageStyle>
+    <>
+      <HistoryPageStyle>
+        <div className={'description'}>Battle history</div>
+        <HistoryListContainer>
+          <div className={'header'}>
+            <div className={'cell'}>No.</div>
+            <div className={'cell'}>Match ID</div>
+            <div className={'cell'}>Level</div>
+            <div className={'cell'}></div>
+          </div>
+          {history &&
+            history.map((res, index) => {
+              return (
+                <div key={index} className={'row'}>
+                  <div className={'cell'}>{index + 1}</div>
+                  <div className={'cell'}>{res.match_id}</div>
+                  <div className={'cell'}>{res.battle_level}</div>
+                  <div className={'cell'}>
+                    <div
+                      className={'btn'}
+                      onClick={() => getHistoryDetails(res.id)}
+                    >
+                      Detail
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </HistoryListContainer>
+      </HistoryPageStyle>
+      {battleDetail && (
+        <BattleDetailModal>
+          <div className={'head'} onClick={() => setBattleDetail(null)}>
+            <div>
+              {battleDetail.summary.winner === 'boss' ? 'Defeat' : 'Winner'}
+            </div>
+            <div>X</div>
+          </div>
+          <HistoryListContainer>
+            <div className={'header black'}>
+              <div className={'cell'}>Attacker</div>
+              <div className={'cell'}>Damage</div>
+              <div className={'cell'}>HP</div>
+              <div className={'cell'}>Max HP</div>
+            </div>
+            {battleDetail &&
+              battleDetail.action_list.map((res: any, index: number) => {
+                return (
+                  <div key={index} className={'row'}>
+                    <div className={'cell'}>{res.attacker}</div>
+                    <div className={'cell'}>{res.damage}</div>
+                    <div className={'cell'}>{res.hp}</div>
+                    <div className={'cell'}>{res.maxHp}</div>
+                  </div>
+                );
+              })}
+          </HistoryListContainer>
+        </BattleDetailModal>
+      )}
+    </>
   );
 };
 HistoryPage.getLayout = function getLayout(page: ReactElement) {
@@ -71,17 +120,73 @@ HistoryPage.getLayout = function getLayout(page: ReactElement) {
 export default HistoryPage;
 
 const HistoryListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+  padding: 16px;
+
   .header {
+    min-width: 800px;
+    width: 100%;
+    height: 40px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    overflow: hidden;
+    transition: 0.15s ease-out;
+    border-radius: 8px;
+    align-items: center;
+    text-align: center;
+    color: #fff;
   }
+
+  .header.black {
+    color: #000;
+    background: #d9d9d9;
+  }
+
   .row {
     min-width: 800px;
     width: 100%;
     height: 40px;
     background: #fdfdfd;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     overflow: hidden;
     transition: 0.15s ease-out;
     border-radius: 8px;
+    align-items: center;
+    text-align: center;
+
+    .btn {
+      background: #ff8237;
+      color: #fff;
+      border-radius: 8px;
+      height: 28px;
+      width: fit-content;
+      justify-self: end;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-inline: 4px;
+      margin-right: 10px;
+      cursor: pointer;
+    }
+  }
+`;
+
+const BattleDetailModal = styled.div`
+  position: absolute;
+  height: fit-content;
+  width: fit-content;
+  background: white;
+  top: 88px;
+  z-index: 99;
+  border-radius: 8px;
+  .head {
+    display: flex;
+    justify-content: space-between;
+    padding: 16px;
+    font-size: 18px;
+    cursor: pointer;
   }
 `;
